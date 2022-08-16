80bd8ab2efa98658c8964f921e6a6a6bfb6307f8
Jared McMahan <jaredm788@gmail.com>
2022-08-16 16:37:15 -0600

    commit tree 52ea162dab1316b96777034ae1dce8e2eaf184a2

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+// first try 
+// function returnFirstTwoDrivers(driver) {
+//     return 
+// }
+
+
+
+// Where is the anonymous function?
+
+function returnFirstTwoDrivers(driverSelect) {
+    const firstDrivers = driverSelect
+    return firstDrivers.slice(0,2)
+ }
+
+ function returnLastTwoDrivers(pickLast) {
+    const lastDrivers = pickLast
+    return lastDrivers.slice(2)
+ }
+
+
+    
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+
+
+
+
+function createFareMultiplier (multiplier) {
+    return function (number) {
+       return  multiplier  * number 
+        
+    }
+}
+
+function fareDoubler(sum) {
+    return sum * 2
+}
+function fareTripler(sum1) {
+    return sum1 * 3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers)
+}
